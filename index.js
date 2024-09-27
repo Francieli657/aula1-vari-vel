ec4501a7f8f0b6f5a4cf0dddf5369db88b0f6148
leandrafernanda <leandrafriedrich@gmail.com>
2024-09-26 22:01:14 -0300

    enviadfo meu codigo

---
+++ index.js
@@ -0,0 +1,10 @@
+// comendando em uma linha 
+/*
+  Comentário de multiplas linhas
+  Aqui podemos ter mais de uma linha
+  de comentário
+*/
+
+let nome = prompt('qual é o seu nome?')
+alert('Seja bem vindo ' + nome +'!')
+console.log('imprimindo no console do navegador')
